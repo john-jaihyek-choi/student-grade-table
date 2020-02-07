aa2f2074c9311c6d9e9cdc13a4691fa5a94aa6be
John Choi <jchoi950@Johns-MacBook-Pro.local>
2020-02-06 16:23:58 -0800

    created grade-form.js with GradeForm class with constructor and 2 methods (onSubmit,handleSubmit), then instantiated them in main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var tableElement = document.querySelector("table");
 var headerElement = document.querySelector("header");
+var formElement = document.querySelector("form");
 var gradeTable = new GradeTable(tableElement);
 var pageHeader = new PageHeader(headerElement);
-var app = new App(gradeTable, pageHeader);
+var gradeForm = new GradeForm(formElement);
+var app = new App(gradeTable, pageHeader, gradeForm);
 app.start();
